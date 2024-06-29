b4a9c355147c0cd888268a4f8e8b441fd131bce8
Matheus Farias <matheusfarias95@hotmail.com>
2024-06-29 16:55:06 -0300

    feat: criação da tabela clientes,implementando uma rota POST para cadastro de clientes

---
+++ src/database/Connection.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+
+class Database {
+  constructor() {
+    this.database = new Pool({
+      user: "postgres",
+      password: "****",
+      host: "localhost",
+      database: "api_labCommerce",
+      port: 5432
+    });
+  }
+}
+
+module.exports = Database;
---
+++ src/routes/clients.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const ClientController = require("../controllers/ClientController");
+
+const clientsRoutes = new Router();
+const clientController = new ClientController();
+
+clientsRoutes.post("/", clientController.criar.bind(clientController));
+clientsRoutes.get("/:id", clientController.pesquisar.bind(clientController));
+
+module.exports = clientsRoutes;
